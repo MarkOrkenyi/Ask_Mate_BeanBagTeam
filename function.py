2914265aaa2fdeec36cd03ac254355698f192bae
MarkOrkenyi <mark.orkenyi@gmail.com>
2017-05-09 14:50:54 +0200

    Added read_csv() and write_csv()

---
+++ function.py
@@ -0,0 +1,16 @@
+def read_csv(filename):
+    '''Opens csv file(filename) and returns a list of lists with it's content called ***"csv_content***"'''
+    csv_content = []
+    with open(filename) as data:
+        content = csv.reader(data, delimiter=',')
+        for row in content:
+            csv_content.append(row)
+    return csv_content
+
+
+def write_csv(filename):
+    '''Writes out the list of lists to a csv file. ***REQUIRED: "csv_content" list of lists!*** '''
+    with open(filename, mode="w") as data:
+        datawriter = csv.writer(data, delimiter=',')
+        for row in csv_content:
+            datawriter.writerow(row)
