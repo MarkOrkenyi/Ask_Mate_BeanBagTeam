1b3bcb7b8cdf51459860de3d59a57e5e1c69e7ca
Annamaria Sojnoczki <soj.annam@gmail.com>
2017-05-10 16:11:08 +0200

    question.py/delete question in progress

--- question.py
+++ question.py
@@ -11,6 +11,24 @@ app = Flask(__name__)
 @app.route('/new_question', methods=['GET', 'POST'])
 def add_new_question():
     if request.method == 'GET':
+        return render_template("question.html")
+
+    elif request.method == 'POST':
+        id_ = function.get_new_id('./data/question.csv', True)
+        submisson_time = datetime.datetime.now()
+        view_number = '0'
+        vote_number = '0'
+        title = request.form["title"]
+        message = request.form["message"]
+        fieldnames = [id_, submisson_time, view_number, vote_number, title, message]
+        function.write_csv('./data/question.csv', fieldnames, True)
+        return redirect("./")
+
+
+"""
+@app.route('/new_question', methods=['GET', 'POST'])
+def add_new_question():
+    if request.method == 'GET':
         print('get')
         return render_template("question.html")
 
@@ -31,12 +49,49 @@ def add_new_question():
         return render_template("question.html")
 
 
+@app.route('/question/<question_id>/edit', methods=['POST', 'GET'])
+def update_question(question_id=None):
+    if request.method == 'GET':
+        edit_data = function.read_csv('./data/question.csv', True)
+        for line in edit_data:
+            print(line)
+            if line[0] == question_id:
+                return render_template("question.html", title=line[4], message=line[5])
+
+    elif request.method == 'POST':
+        edit_title = request.form["title"]
+        edit_message = request.form["message"]
+        edit_data = function.read_csv('./data/question.csv', True)
+        fieldnames = []
+        for line in edit_data:
+            if edit_data[0] == question_id:
+                fieldnames.append(edit_data[0])
+                fieldnames.append(edit_data[1])
+                fieldnames.append(edit_data[2])
+                fieldnames.append(edit_data[3])
+        fieldnames.append(edit_title, edit_message)
+        print(fieldnames)
+        update_question = update(function.readcsv('./data/question.csv', True), id_, fieldnames)
+        write_csv('/data/question.csv', update_question)
+        return render_template("question.html", question_id=question_id)
+
+
+def update(file_data, id_, field):
+    for index, elements in enumerate(file_data):
+        if elements[0] == id_:
+            update_data = [n for n in field]
+            file_data.pop(index)
+            file_data.append(update_data)
+    return file_data""""
+
+
+@app.route('/question/<question_id>/delete', methods=['GET', 'POST'])
 def delete_question(question_id):
-    id_ = request.form['delete_question']
+    if request.method == 'POST'
+    id_ = question_id
     delete_data = remove(function.read_csv('./data/question.csv', True), id_)
-    function.write_csv('./data/question.csv', delete_data, True)
-    list_data = function.read_csv('./data/question.csv')
-    return render_template("list.html", list_data=list_data)
+    function.write_csv('./data/question.csv', delete_data, True, False)
+    return rediect('./list')
 
 
 def remove(file_data, id_):
