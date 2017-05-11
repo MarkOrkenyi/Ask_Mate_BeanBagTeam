21251d4dc9da0d56d3c89932d0b6fc8d70277fbb
MarkOrkenyi <mark.orkenyi@gmail.com>
2017-05-11 21:14:07 +0200

    Added comments, refactored functions

--- function.py
+++ function.py
@@ -4,11 +4,11 @@ import time
 import datetime
 
 
-def read_csv(filename, type_of_csv):
-    '''Opens csv file(filename) and returns a list of lists with it's content
-    \nUse: read_csv(<filename>,type_of_csv can be "question" or "answer"'''
+def read_csv(filepath, type_of_csv):
+    ''''Opens csv file and returns a list of lists with it's content
+    \nUse: read_csv(<filepath>,type_of_csv can be "question" or "answer"'''
     csv_content = []
-    with open(filename, mode="r") as data:
+    with open(filepath, mode="r") as data:
         content = csv.reader(data, delimiter=',')
         for row in content:
             csv_content.append(row)
@@ -17,7 +17,8 @@ def read_csv(filename, type_of_csv):
 
 
 def write_csv(csv_path, csv_data, type_of_csv):
-    '''csv_data is the FULL list of lists of the csv file, type_of_csv can be "question" or "answer"'''
+    '''Encodes the csv_data with encode(), then writes it out to the given csv_path.
+    \nUse:<csv_path>,<csv_data> is the FULL list of lists of the csv file, <type_of_csv> can be "question" or "answer"'''
     csv_data = encode(csv_data, type_of_csv)
     with open(csv_path, mode="w") as data:
         datawriter = csv.writer(data, delimiter=',')
@@ -26,8 +27,9 @@ def write_csv(csv_path, csv_data, type_of_csv):
 
 
 def encode(csv_data, type_of_csv):
+    '''Encodes the csv_data'''
     for row in csv_data:
-        row[1] = convert_time(row[1], "string")
+        row[1] = convert_time(row[1], "encode")
         row[4] = bytes.decode(base64.b64encode(str.encode(row[4])))
         if type_of_csv == "question":
             row[5] = bytes.decode(base64.b64encode(str.encode(row[5])))
@@ -35,8 +37,9 @@ def encode(csv_data, type_of_csv):
 
 
 def decode(csv_data, type_of_csv):
+    '''Decodes the csv_data'''
     for row in csv_data:
-        row[1] = convert_time(row[1], "unix")
+        row[1] = convert_time(row[1], "decode")
         row[4] = bytes.decode(base64.b64decode(row[4]))
         if type_of_csv == "question":
             row[5] = bytes.decode(base64.b64decode(row[5]))
@@ -44,28 +47,31 @@ def decode(csv_data, type_of_csv):
 
 
 def convert_time(input_, type_):
-    if type_ == "string":
-        dt_time_e = datetime.datetime.strptime(str(input_), ("%Y-%m-%d %H:%M:%S"))
-        tuple_ = dt_time_e.timetuple()
+    '''Converts time from UNIX to readable format, or vice-versa'''
+    if type_ == "encode":
+        datetime_obj = datetime.datetime.strptime(str(input_), ("%Y-%m-%d %H:%M:%S"))
+        tuple_ = datetime_obj.timetuple()
         return time.mktime(tuple_)
-    elif type_ == "unix":
-        dt_time = datetime.datetime.fromtimestamp(float(input_))
-        return datetime.datetime.strftime(dt_time, ("%Y-%m-%d %H:%M:%S"))
+    elif type_ == "decode":
+        datetime_obj = datetime.datetime.fromtimestamp(float(input_))
+        return datetime.datetime.strftime(datetime_obj, ("%Y-%m-%d %H:%M:%S"))
 
 
-def get_new_id(filename, type_of_csv):
+def get_new_id(filepath, type_of_csv):
+    '''Returns a new ID for the entry. If no entry is present, the first ID will be 1'''
     id_ = ['0']
-    data = read_csv(filename, type_of_csv)
+    data = read_csv(filepath, type_of_csv)
     for row in data:
         id_.append(row[0])
     max_id = max(map(int, id_))
     return (int(max_id) + 1)
 
 
-def remove(file_data, id_, num):
+def remove(file_data, id_, index_):
+    '''Removes the matching row from the csv_data, and returns csv_data'''
     to_pop = []
     for index, elements in enumerate(file_data):
-        if elements[num] == id_:
+        if elements[index_] == id_:
             to_pop.append(index)
     for i in reversed(list(map(int, to_pop))):
         file_data.pop(i)
--- main.py
+++ main.py
@@ -26,6 +26,7 @@ def show_list():
 
 @app.route("/question/<question_id>")
 def question_details(question_id):
+    '''Renders question_details.html with the details of a given question'''
     questions_list = function.read_csv("./data/question.csv", "question")
     for row in questions_list:
         if question_id == row[0]:
@@ -61,12 +62,14 @@ def question_details(question_id):
 
 @app.route('/newquestion', methods=['GET', 'POST'])
 def add_new_question():
+    '''Renders question.html to get a new question, then writes that out to the csv file
+    \nRedirects to the questions list page'''
     if request.method == 'GET':
         return render_template("question.html")
 
     if request.method == "POST":
         id_ = function.get_new_id('./data/question.csv', "question")
-        submisson_time = function.convert_time(time.time(), "unix")
+        submisson_time = function.convert_time(time.time(), "encode")
         view_number = '0'
         vote_number = '0'
         title = request.form["title"]
@@ -80,6 +83,7 @@ def add_new_question():
 
 @app.route('/question/<question_id>/delete', methods=['GET', 'POST'])
 def delete_question(question_id):
+    '''Deletes a given question, then redirects to "./list"'''
     if request.method == 'POST':
         id_ = question_id
         delete_data = function.remove(function.read_csv('./data/question.csv', "question"), id_, 0)
@@ -91,6 +95,8 @@ def delete_question(question_id):
 
 @app.route("/question/<question_id>/new-answer", methods=['POST', 'GET'])
 def new_answer(question_id):
+    '''Renders answer.html to get a new answer, then writes that out to a csv file
+    \nRedirects to the given question's detail page'''
     if request.method == 'GET':
         return render_template("answer.html", question_id=question_id)
 
@@ -98,7 +104,7 @@ def new_answer(question_id):
         to_add = []
         answer = str(request.form['newanswer'])
         to_add.append(function.get_new_id("./data/answer.csv", "answer"))
-        to_add.append(function.convert_time(time.time(), "unix"))
+        to_add.append(function.convert_time(time.time(), "decode"))
         to_add.append(0)
         to_add.append(question_id)
         to_add.append(answer)
@@ -110,6 +116,8 @@ def new_answer(question_id):
 
 @app.route("/question/<question_id>/edit", methods=['POST', 'GET'])
 def edit_question(question_id):
+    '''Renders question.html to edit a given question, then updates the question in the csv file
+    \n Redirects to the question's detail page'''
     if request.method == 'GET':
         question_list = function.read_csv("./data/question.csv", "question")
         for row in question_list:
@@ -139,6 +147,7 @@ def edit_question(question_id):
 
 @app.route('/question/<question_id>/vote-up', methods=['POST', 'GET'])
 def question_vote_up(question_id):
+    '''Increases the vote count of the given question'''
     to_add = []
     csv_data = function.read_csv('./data/question.csv', "question")
     for row in csv_data:
@@ -158,6 +167,7 @@ def question_vote_up(question_id):
 
 @app.route('/question/<question_id>/vote-down', methods=['POST', 'GET'])
 def question_vote_down(question_id):
+    '''Decreases the vote count of the given question'''
     to_add = []
     csv_data = function.read_csv('./data/question.csv', "question")
     for row in csv_data:
@@ -177,6 +187,7 @@ def question_vote_down(question_id):
 
 @app.route('/answer/<answer_id>/vote-up', methods=['POST', 'GET'])
 def answer_vote_up(answer_id):
+    '''Increases the vote count of the given answer'''
     question_id = request.form['questionid']
     to_add = []
     csv_data = function.read_csv('./data/answer.csv', "answer")
@@ -197,6 +208,7 @@ def answer_vote_up(answer_id):
 
 @app.route('/answer/<answer_id>/vote-down', methods=['POST', 'GET'])
 def answer_vote_down(answer_id):
+    '''Decreases the vote count of the given answer'''
     question_id = request.form['questionid']
     to_add = []
     csv_data = function.read_csv('./data/answer.csv', "answer")
@@ -217,6 +229,7 @@ def answer_vote_down(answer_id):
 
 @app.route('/answer/<answer_id>/delete', methods=['GET', 'POST'])
 def delete_answer(answer_id):
+    '''Deletes given answer, then redirects to the question's detail page'''
     question_id = request.form['questionid']
     answer_delete_data = function.remove(function.read_csv('./data/answer.csv', "answer"), answer_id, 0)
     function.write_csv('./data/answer.csv',  answer_delete_data, "answer")
